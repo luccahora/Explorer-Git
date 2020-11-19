94d063cf6e5b1881239829cf3f785b558e4352a8
Lucca Hora <luccahora@gmail.com>
2020-11-19 20:52:15 -0300

    Tela Dash

--- gitexplorer/src/pages/Dashboard/index.tsx
+++ gitexplorer/src/pages/Dashboard/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { FiArrowUpRight, FiChevronRight } from "react-icons/fi";
+
 import logoImg from "../../assets/logo.svg";
 
-import { Title, Form } from "./style";
+import { Title, Form, Repositories } from "./style";
 
 const Dashboard: React.FC = () => {
   return (
@@ -13,6 +15,23 @@ const Dashboard: React.FC = () => {
         <input type="text" placeholder="Digite o nome do repositório " />
         <button type="submit"> Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/39227316?s=460&u=783c76c9fc8fa08ca97b1f8e05c575599c89df76&v=4"
+            alt="Lucca"
+          />
+          <div>
+            <strong>Rockeseat</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+              reprehenderit accusantium! Labore, dicta accusantium molestias
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
--- gitexplorer/src/pages/Dashboard/style.ts
+++ gitexplorer/src/pages/Dashboard/style.ts
@@ -43,4 +43,60 @@ export const Form = styled.form `
             background:${shade(0.2, '#04D361')}
         }
     }
+
+`;
+
+export const Repositories = styled.div `
+    margin-top: 80px;
+    max-width: 700px;
+
+    a {
+        background: #FFF;
+        border-radius: 5px;
+        width: 100%;
+        padding: 24px;
+        display: block;
+        text-decoration: none;
+
+        display: flex;
+        align-items: center;
+
+        transition: transform 0.2s;
+
+        & + a {
+            margin-top: 16px;
+        }
+
+        &:hover{
+            transform: translateX(10px)
+        }
+
+        img {
+            width: 64px;
+            height: 64px;
+            border-radius: 50%;
+        }
+
+        div {
+            margin-left: 16px;
+
+            strong {
+                font-size: 20px;
+                color: #3D3D4D;
+            }
+
+            p {
+                font-size: 16px;
+                color: #a8a8b3;
+                margin-top: 4px;
+            }
+        }
+
+        svg {
+            margin-left: auto;
+            color: #CBCBD6;
+
+        }
+        
+    }
 `;
