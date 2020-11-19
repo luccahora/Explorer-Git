2b208211c6959ee93fe96d8071511016a27c528e
Lucca Hora <luccahora@gmail.com>
2020-11-19 20:05:02 -0300

    Stylo global

---
+++ gitexplorer/src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Title } from './style';
+
+const Dashboard: React.FC = () =>{
+    return(
+        <Title> Explore repositórios no GitHub </Title>
+    );
+}
+
+export default Dashboard;
