0693f26500d21d2189407c395955d5f828dc81c4
Lucca Hora <luccahora@gmail.com>
2020-11-21 19:52:25 -0300

    page repository

--- gitexplorer/src/pages/Repository/index.tsx
+++ gitexplorer/src/pages/Repository/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouteMatch, Link } from "react-router-dom";
-import { Header } from "./styles";
-import { FiChevronsLeft } from "react-icons/fi";
+import { Header, RepositoryInfo, Issues } from "./styles";
+import { FiChevronsLeft, FiChevronRight } from "react-icons/fi";
 
 import logoImg from "../../assets/logo.svg";
 
@@ -13,13 +13,49 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
   return (
-    <Header>
-      <img src={logoImg} alt="Github Explorer" />
-      <Link to="/Dashboard">
-        <FiChevronsLeft size={16} />
-        Voltar
-      </Link>
-    </Header>
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars3.githubusercontent.com/u/39227316?s=460&u=783c76c9fc8fa08ca97b1f8e05c575599c89df76&v=4" />
+          <div>
+            <strong>vuejs/vue</strong>
+            <p>Descricao do projeto</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>18</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="teste">
+          <div>
+            <strong>dasdas</strong>
+            <p>asdas</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
